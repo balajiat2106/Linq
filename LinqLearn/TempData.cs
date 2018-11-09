d2e1b4af6da4b159388dc1ae5fe51e350d247a3d
Balaji Prabakaran <balaji.prabakaran@SGZ.com>
2018-11-09 20:02:52 +0530

    Added temp data

---
+++ LinqLearn/TempData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqLearn
+{
+    class TempData
+    {
+        List<Employee> employees = new List<Employee>() {
+            new Employee{EmpID=1,EmpName="Balaji",Designation="Tech Lead"},
+            new Employee{EmpID=2, EmpName="Rakesh",Designation="QA Lead"}
+        };
+
+        private class Employee
+        {
+            public int EmpID { get; set; }
+            public string EmpName { get; set; }
+            public string Designation { get; set; }
+        }
+    }
+}
